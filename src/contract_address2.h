8b488c9a07c1fe61a85bd21c22c681d9d8e20ddf
Mikhail Samin <i@mishasamin.xyz>
2024-11-17 18:14:47 +0000

    Update contract_address2.h

--- src/contract_address2.h
+++ src/contract_address2.h
@@ -64,7 +64,9 @@ __device__ uint32_t mask_value(_uint256 mask, int index) {
 
 
 __global__ void __launch_bounds__(BLOCK_SIZE, 2) gpu_contract2_address_work(
-    int score_method, Address a, _uint256 prefix_uint256, int prefix_bits, _uint256 base_random_key, _uint256 bytecode) {
+    int score_method, Address a, _uint256 prefix_uint256, int prefix_bits,
+    _uint256 base_random_key, _uint256 bytecode) {
+
     uint64_t thread_id = (uint64_t)threadIdx.x + (uint64_t)blockIdx.x * (uint64_t)BLOCK_SIZE;
     uint64_t key_offset = (uint64_t)THREAD_WORK * thread_id;
 
@@ -78,23 +80,27 @@ __global__ void __launch_bounds__(BLOCK_SIZE, 2) gpu_contract2_address_work(
         "addc.cc.u32 %5, %5, 0x0;   \n\t"
         "addc.cc.u32 %6, %6, 0x0;   \n\t"
         "addc.u32 %7, %7, 0x0;      \n\t"
-        : "+r"(key.h), "+r"(key.g), "+r"(key.f), "+r"(key.e), "+r"(key.d), "+r"(key.c), "+r"(key.b), "+r"(key.a) : "r"((uint32_t)(key_offset & 0xFFFFFFFF)), "r"((uint32_t)(key_offset >> 32))
+        : "+r"(key.h), "+r"(key.g), "+r"(key.f), "+r"(key.e), "+r"(key.d), "+r"(key.c), "+r"(key.b), "+r"(key.a)
+        : "r"((uint32_t)(key_offset & 0xFFFFFFFF)), "r"((uint32_t)(key_offset >> 32))
     );
-    
+
     int random_bits = 256 - prefix_bits;
-    _uint256 shifted_random_bits = cpu_shift_left_256(key, prefix_bits);
-    
-    _uint256 salt = cpu_or_256(prefix_uint256, shifted_random_bits);
-    
+
     for (int i = 0; i < THREAD_WORK; i++) {
-        handle_output2(score_method, calculate_contract_address2(a, salt, bytecode), key_offset + i);
-    
-        // Increment the random bits portion of the salt
-        increment_random_bits_in_salt(&salt, prefix_bits);
-    
-        // Alternatively, increment 'key' and recombine
-        key.h += 1; // Increment the least significant part of the random bits
-        shifted_random_bits = cpu_shift_left_256(key, prefix_bits);
-        salt = cpu_or_256(prefix_uint256, shifted_random_bits);
+        // Shift key to align random bits after the prefix
+        _uint256 shifted_random_bits = cpu_shift_left_256(key, prefix_bits);
+
+        // Combine prefix and shifted random bits to form the full salt
+        _uint256 salt = cpu_or_256(prefix_uint256, shifted_random_bits);
+
+        // Compute the contract address using the salt
+        Address addr = calculate_contract_address2(a, salt, bytecode);
+
+        // Call handle_output2 with the salt
+        handle_output2(score_method, addr, salt);
+
+        // Increment key for the next iteration
+        key.h += 1;
     }
 }
+
