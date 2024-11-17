9df941a728c2c57fb12b030955ea9a582a3b1c8e
Mikhail Samin <i@mishasamin.xyz>
2024-11-17 18:06:19 +0000

    Update contract_address2.h

--- src/contract_address2.h
+++ src/contract_address2.h
@@ -20,11 +20,12 @@
 #include "math.h"
 
 
-__global__ void __launch_bounds__(BLOCK_SIZE, 2) gpu_contract2_address_work(int score_method, Address a, _uint256 base_key, _uint256 bytecode) {
+__global__ void __launch_bounds__(BLOCK_SIZE, 2) gpu_contract2_address_work(
+    int score_method, Address a, _uint256 prefix_uint256, int prefix_bits, _uint256 base_random_key, _uint256 bytecode) {
     uint64_t thread_id = (uint64_t)threadIdx.x + (uint64_t)blockIdx.x * (uint64_t)BLOCK_SIZE;
     uint64_t key_offset = (uint64_t)THREAD_WORK * thread_id;
 
-    _uint256 key = base_key;
+    _uint256 key = base_random_key;
     asm(
         "add.cc.u32 %0, %0, %8;     \n\t"
         "addc.cc.u32 %1, %1, %9;    \n\t"
@@ -36,8 +37,21 @@ __global__ void __launch_bounds__(BLOCK_SIZE, 2) gpu_contract2_address_work(int 
         "addc.u32 %7, %7, 0x0;      \n\t"
         : "+r"(key.h), "+r"(key.g), "+r"(key.f), "+r"(key.e), "+r"(key.d), "+r"(key.c), "+r"(key.b), "+r"(key.a) : "r"((uint32_t)(key_offset & 0xFFFFFFFF)), "r"((uint32_t)(key_offset >> 32))
     );
+    
+    int random_bits = 256 - prefix_bits;
+    _uint256 shifted_random_bits = cpu_shift_left_256(key, prefix_bits);
+    
+    _uint256 salt = cpu_or_256(prefix_uint256, shifted_random_bits);
+    
     for (int i = 0; i < THREAD_WORK; i++) {
-        handle_output2(score_method, calculate_contract_address2(a, key, bytecode), key_offset + i);
-        key.h += 1;
+        handle_output2(score_method, calculate_contract_address2(a, salt, bytecode), key_offset + i);
+    
+        // Increment the random bits portion of the salt
+        increment_random_bits_in_salt(&salt, prefix_bits);
+    
+        // Alternatively, increment 'key' and recombine
+        key.h += 1; // Increment the least significant part of the random bits
+        shifted_random_bits = cpu_shift_left_256(key, prefix_bits);
+        salt = cpu_or_256(prefix_uint256, shifted_random_bits);
     }
 }
